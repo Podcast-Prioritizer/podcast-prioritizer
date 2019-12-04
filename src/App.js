ed2815f1b53c899d4dc3756bdd2917999097dce1
unifolia <james@electronicmail.ca>
2019-12-04 16:52:06 -0500

    :rocket::performing_arts::guitar:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      directionInfo: {},
+      endLocation: "",
+    };
+  };
+
+  componentDidMount() {
+    Axios({
+      url: "https://www.mapquestapi.com/directions/v2/route",
+      method: "GET",
+      dataType: "json",
+      params: {
+        key: "uMDO6BJLrXNNrJI5BZ7A0tFS6AojdBjn",
+        from: "Toronto",
+        to: "Ottawa",
+        routeType: "fastest",
+      },
+    }).then((data) => {
+      this.setState({
+        directionInfo: data.data.route
+      });
+    });
+  };
+  
+  getMapInfo = (event) => {
+    event.preventDefault(event);
+    this.setState({
+      endLocation: "",
+    });
+
+    console.log(this.state.directionInfo.formattedTime);
+    console.log(this.state.directionInfo.locations[0].adminArea5);
+    console.log(this.state.directionInfo.locations[1].adminArea5);
+  };
+
+  render(){
+    return(
+
+      <form onSubmit={this.getMapInfo}>
+
+        <input type="text" placeholder="lmao" />
+
+        <button type="submit">Search</button>
+
+      </form>
+
+    );
+  };
+};
 
 export default App;
