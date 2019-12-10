51eaf157adbebfac8cb0e4b837fe835778ba0343
Russell Bentulan <russellbentulan@gmail.com>
2019-12-10 12:45:25 -0500

    Updates

---
+++ precache-manifest.2849f01973bddb5f393ac3f8f1ff4882.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9878d02651a1685dc1cbcc65f84dd342",
+    "url": "/podcast-prioritizer/index.html"
+  },
+  {
+    "revision": "139af39ad24a26c30279",
+    "url": "/podcast-prioritizer/static/css/main.da6a8aae.chunk.css"
+  },
+  {
+    "revision": "5604aa1b51f20a2579e0",
+    "url": "/podcast-prioritizer/static/js/2.b2ba7d2f.chunk.js"
+  },
+  {
+    "revision": "139af39ad24a26c30279",
+    "url": "/podcast-prioritizer/static/js/main.e9eade8b.chunk.js"
+  },
+  {
+    "revision": "5d232621611e2aab6de1",
+    "url": "/podcast-prioritizer/static/js/runtime-main.631ad712.js"
+  },
+  {
+    "revision": "e0d8a2b9b22ca4f11b0fe39c69e487f9",
+    "url": "/podcast-prioritizer/static/media/girlWalkingListening.e0d8a2b9.jpg"
+  },
+  {
+    "revision": "0b2aa3629c1fcf4cfafc1910b7ed1272",
+    "url": "/podcast-prioritizer/static/media/logo.0b2aa362.png"
+  },
+  {
+    "revision": "a05f1fb32996567bf53cc6d415cc5e55",
+    "url": "/podcast-prioritizer/static/media/sayagata-400px.a05f1fb3.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/podcast-prioritizer/precache-manifest.2849f01973bddb5f393ac3f8f1ff4882.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/podcast-prioritizer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
